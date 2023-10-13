0d66a6ce0b2f26590aa9e89b43a6112491345de5
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2023-10-13 14:01:48 +0000

    Fri, Oct 13, 2023, 7:31 PM +05:30

--- Lecture-/hello1.c
+++ Lecture-/hello1.c
@@ -5,7 +5,7 @@ int main()
     // This is comment
     int num = 5;
     float fl = 3.0;
-    printf("This is integer %n/nThis is float %f/n", num , fl)
+    printf("This is to add" ADD "this is to add/n")
     printf("Hello, World\n");
     return 0;
 }
