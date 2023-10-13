0fcf28fe4206a86e13bb93838fa7757d03327b23
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2023-10-13 09:35:33 +0000

    Fri, Oct 13, 2023, 3:05 PM +05:30

--- Lecture-/hello.c
+++ Lecture-/hello.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    long l = get_long("Enter your name: ");
-    printf("Hello,  %li.\n", l);
+    string s = get_string("Enter your name: ");
+    printf("Hello, %s.\n", s);
 }
