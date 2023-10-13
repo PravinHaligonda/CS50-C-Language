6e07c961b740c9c164513d06af58184c637369c2
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2023-10-13 13:59:09 +0000

    Fri, Oct 13, 2023, 7:29 PM +05:30

--- Lecture-/hello1.c
+++ Lecture-/hello1.c
@@ -4,6 +4,8 @@ int main()
 {
     // This is comment
     int num = 5;
+    float fl = 3.0;
+    printf("This is integer %n/nThis is float %f/n", num , fl)
     printf("Hello, World\n");
     return 0;
 }
