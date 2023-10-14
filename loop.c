ef3408253314d22ca5c272c58f34798cfda5523e
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2023-10-14 08:00:42 +0000

    Sat, Oct 14, 2023, 1:30 PM +05:30

--- loop.c
+++ loop.c
@@ -3,17 +3,18 @@
 
 int main(void)
 {
-    int i = 0;
-    while (i < 3)
+    // int i = 0;
+    // while (i < 3)
+    // {
+    //     printf("Meow\n");
+    //     i++;
+    // }
+
+
+    for (int i = 0; i < 3; i++)
     {
         printf("Meow\n");
-        i++;
     }
-}
-
-// For Loop
-
-int for_loop()
-{
     
 }
+
