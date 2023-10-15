ca37840c89ed43ce797ba882830a018c77b19257
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2023-10-15 05:55:32 +0000

    Sun, Oct 15, 2023, 11:25 AM +05:30

--- Problem_set1/mario.c
+++ Problem_set1/mario.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
+
 int main(void)
 {
-
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
