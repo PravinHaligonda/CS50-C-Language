05ade1927bb3d725595db99c4f36bd8c74f2fd36
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2023-10-19 02:36:53 +0000

    Thu, Oct 19, 2023, 8:06 AM +05:30

--- scores.c
+++ scores.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
 
-    printf("Address of score1: %p\n", &score1);
-    printf("Average: %f\n", (score1 + score2 + score3) / (float) 3);
+    printf("Average: %i\n", (scores[3] / 3));
 
 }
