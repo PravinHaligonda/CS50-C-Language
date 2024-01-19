6ea1b0c702f090948f13f2dc1183d073f65cfb61
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-19 13:05:12 +0000

    Fri, Jan 19, 2024, 6:35 PM +05:30

---
+++ C/conditional.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int x = get_int("X: ");
+    int y = get_int("Y: ");
+    if (x < y)
+    {
+        printf("X is greater than y");
+    } else {
+        printf("X is smaller that y");
+    }
+}
