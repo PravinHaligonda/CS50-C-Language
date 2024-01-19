41e57a630edfd51c02dd8565a47f5110310c7220
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-19 13:18:50 +0000

    Fri, Jan 19, 2024, 6:48 PM +05:30

---
+++ C/compare.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int x = get_int("What's X? ");
+    int y = get_int("What's Y? ");
+
+    if (x < y)
+    {
+        printf("X is less than y\n");
+    }
+}
