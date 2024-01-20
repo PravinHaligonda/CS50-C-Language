a358ebc4e304e6500e1c6c22578938f74112b865
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-20 15:54:40 +0000

    Sat, Jan 20, 2024, 9:24 PM +05:30

--- arrays/scores.c
+++ arrays/scores.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
-{
-    int scores1 = 73;
-    int scores2 = 78;
-    int scores3 = 35;
+// int main(void)
+// {
+//     int scores1 = 73;
+//     int scores2 = 78;
+//     int scores3 = 35;
 
-    printf("Average: %.5f\n", (scores1 + scores2 + scores3) / 3.0);
-}
+//     printf("Average: %.5f\n", (scores1 + scores2 + scores3) / 3.0);
+// }
