8789e78913bcf9cb6463ffbbcad9499a9b136445
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-20 09:55:52 +0000

    Sat, Jan 20, 2024, 3:25 PM +05:30

--- C/meow.c
+++ C/meow.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
+
+void meow(void)
+{
+    printf("meow\n");
+}
+
+
 int main(void)
 {
     int i = 0;
-    while (i < 3)
+    for (i = 0; i < 3; i++;)
     {
-        printf("meow\n");
-        i++;
+        meow()
     }
 }
+
