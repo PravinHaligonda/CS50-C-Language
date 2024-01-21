f204450aa1af76858b0614755934ebbf5792b7db
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-21 12:57:21 +0000

    Sun, Jan 21, 2024, 6:27 PM +05:30

--- memory/memory.c
+++ memory/memory.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-int nv01(int);
-
 int main(void)
 {
     int n = 3;
     int *p = &n; // * is a pointer to store the address of variable n; and & is a address at which the variable to choose.
     printf("N: %p\n", p); // %p to place the pointer
-    nv01();
 }
 
 /* It is like go to the address of n and tell me what is there. */
 
-
-int nv01(int n)
-{
-    int n = 50;
-    int *p = &n;
-    printf("N: %i\n", *p);
-}
