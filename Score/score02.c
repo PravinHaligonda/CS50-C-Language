799a68158ac5bff60dc857d98e84de20d2d8418f
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-21 03:26:21 +0000

    Sun, Jan 21, 2024, 8:56 AM +05:30

--- Score/score02.c
+++ Score/score02.c
@@ -1,12 +1,22 @@
+// Solution:
+/*
+
+Use the arrays
+
+*/
+
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int scores[3];
 
-    scores[0] = 73;
-    scores[1] = 72;
-    scores[2] = 33;
+    // Adding some feature.
+    scores[0] = get_int("Score: ");
+    scores[1] = get_int("Score: ");
+    scores[2] = get_int("Score: ");
 
     printf("Average %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
+
