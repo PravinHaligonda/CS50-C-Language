0ddbe26738ba89df2337d6cdf7128bb8f1d604ab
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-21 09:52:59 +0000

    Sun, Jan 21, 2024, 3:22 PM +05:30

---
+++ Algorithm/phonebook.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+
+int main(void)
+{
+    string names[] = {"John", "Carter", "Hello"};
+    string numbers[] = {"0000001", "0000002", "0000003"};
+
+    string name = get_string("Name: ");
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+}
