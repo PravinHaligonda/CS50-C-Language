baa63ba27cba320342f69b443f63ef737c625e4f
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-21 04:20:09 +0000

    Sun, Jan 21, 2024, 9:50 AM +05:30

---
+++ Score/score_final.c
@@ -0,0 +1,28 @@
+// Averages three numbers using arrays, constants and helper function.
+
+#include <cs50.h>
+#include <stdio.h>
+
+// Constants
+const int N = 3;
+
+
+int main(void)
+{
+    int scores[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
+    printf("Average: %f\n",);
+}
+
+float average(int length, int array[])
+{
+    sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        
+    }
+}
