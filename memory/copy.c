4df7a4537febe0accaf5a562cd63ed2e5ad2a206
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 05:34:46 +0000

    Tue, Jan 23, 2024, 11:04 AM +05:30

--- memory/copy.c
+++ memory/copy.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
@@ -11,5 +12,7 @@ int main(void)
     printf("S's Address: %p\n", &s[0]);
     printf("T: %c\n", t[0]);
     printf("T's Address: %p\n", &t[0]);
+    t[0] = toupper(t[0]);
+    printf("T's: %s\n", t);
 
 }
