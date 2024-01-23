c5f25511a76b3ee6a4f7f7e8c7edae67fb0e9bf2
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 09:08:18 +0000

    Tue, Jan 23, 2024, 2:38 PM +05:30

--- memory_v2/garbage.c
+++ memory_v2/garbage.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int garbage(void);
-
 int main(void)
 {
-    int x[1024];
+    int p = 13;
+    int *a = &p;
 
-    for (int i = 0; i < 1024; i++)
-    {
-        printf("%i\n", x[i]);
-    }
-    garbage();
-}
-
-// Solution:
-
-int garbage(void)
-{
-    int n = 13;
-    int *x = &n;
-
-    printf("X: %i\n", x);
+    printf("A: %i\n", a);
 }
