46446ba5dc12f980521489d34c97c3fc73cc30ae
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 16:33:32 +0000

    Tue, Jan 23, 2024, 10:03 PM +05:30

--- pointers/address.c
+++ pointers/address.c
@@ -3,12 +3,6 @@
 
 int main(void)
 {
-    string s = "Pravin";
-
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
-    printf("%p\n", &s[4]);
-    printf("%p\n", &s[5]);
+    char *s = "Pravin";
+    printf(")
 }
