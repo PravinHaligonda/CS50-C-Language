2765af376f42aab563f448529946719b62ecc7af
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 06:23:23 +0000

    Tue, Jan 23, 2024, 11:53 AM +05:30

--- memory/copy_02.c
+++ memory/copy_02.c
@@ -9,22 +9,24 @@ int main(void)
     // Taking inputs from the user.
     char *s = get_string("s: ");
 
-    
+    // Allocate the memory for variable t.
     char *t = malloc(strlen(s) + 1);
 
-
+    // copy every character in var s and '0\n' null character.
     for (int i = 0, n = strlen(s); i <= n; i++)
     {
         t[i] = s[i];
     }
 
-
+    // Check for valid input length.
     if (strlen(t) > 0)
     {
         t[0] = toupper(t[0]);
     }
 
-
+    // Show it
     printf("S: %s\n", s);
+    printf("S: %p\n", &s);
     printf("T: %s\n", t);
-}
+    printf("T: %p\n", &s);
+    }
