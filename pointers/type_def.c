24af45848bcd8a4a52f2a7def889223f1bf2a258
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 17:08:55 +0000

    Tue, Jan 23, 2024, 10:38 PM +05:30

--- pointers/type_def.c
+++ pointers/type_def.c
@@ -5,8 +5,11 @@ typedef struct
 {
     char *fname;
     char *lname;
+    int year;
 }
 fullname;
+
+int age(int n);
 
 int main(void)
 {
@@ -14,9 +17,16 @@ int main(void)
 
     char *fst = get_string("First Name: ");
     char *lst = get_string("Last Name: ");
+    int year = get_int("Age: ");
 
     fn.fname = fst;
     fn.lname = lst;
 
     printf("Fullname: %s %s\n", fn.fname, fn.lname);
+    age(year);
+
+int age(int n)
+{
+    printf("You are %i years old.", n);
+    return 0;
 }
