ba3af49c19e15b668e496605a0b1e01cb3ec1bcc
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 06:08:10 +0000

    Tue, Jan 23, 2024, 11:38 AM +05:30

--- memory/copy_02.c
+++ memory/copy_02.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
 
-    char *t = s;
+    char *t = malloc(strlen(s) + 1);
 
     if (strlen(t) > 0)
     {
