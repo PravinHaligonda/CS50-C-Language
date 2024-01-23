5f4f1d541bce7e5fb126c6d203c217d8d8ef3750
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 16:50:11 +0000

    Tue, Jan 23, 2024, 10:20 PM +05:30

---
+++ pointers/type_def.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+typedef struct
+{
+    string fname;
+    string lname;
+}
+fullname;
+
+int main(void)
+{
+    string fullname[3];
+
+    fullname[0].fname = "Pravin";
+    fullname[0].lname = "Haligonda";
+
+    printf("Fullname: )
+}
