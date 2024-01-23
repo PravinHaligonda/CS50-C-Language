435e6ffca4cd2838ae8f496b956115e75ed72fe8
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-23 05:24:21 +0000

    Tue, Jan 23, 2024, 10:54 AM +05:30

--- memory/copy.c
+++ memory/copy.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
-    int s = get_string("S: %s\n", s);
-    
+    string s = get_string("S: %s\n", s);
+
+    string t = s;
+
+    t[0] = toupper(t[0]);
+
+
+    printf("S: %s\n", s);
+    printf("t: %s\n", t);
+
 }
