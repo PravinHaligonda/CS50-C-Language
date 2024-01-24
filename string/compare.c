1cf1418c68833b98d8152a0d6bb1ea1d7954db7f
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-24 03:34:00 +0000

    Wed, Jan 24, 2024, 9:04 AM +05:30

---
+++ string/compare.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    char *a = get_string("A: ");
+    char *b = get_string("B: ");
+
+    if (strcmp(a, b) == 0)
+    {
+        printf("Same.\n:");
+    }
+    else
+    {
+        printf("Different.\n");
+    }
+}
+
+// For numbers
+int num(int a, int b)
+{
+    if (a == b)
+}
