4cc014df01ee29547ad81e3069422a706b412cc9
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-24 08:19:13 +0000

    Wed, Jan 24, 2024, 1:49 PM +05:30

--- Io/phonebook.c
+++ Io/phonebook.c
@@ -9,7 +9,7 @@ int main(void)
     char *name = get_string("Name: ");
     char *number = get_string("Number: ");
 
-    fprintf(file, "%s %s\n",name, number);
+    fprintf(file, "%s, %s\n",name, number);
 
    fclose(file);
 }
