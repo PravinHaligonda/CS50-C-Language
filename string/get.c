54bbb8468efb03e2c5a9731dcf9321b531ad99fb
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-24 07:37:15 +0000

    Wed, Jan 24, 2024, 1:07 PM +05:30

--- string/get.c
+++ string/get.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int n;
-    printf("Number: ");
-    scanf("%i", &n);
-    printf("Number: %i\n", n);
-}
+// int main(void)
+// {
+//     int n;
+//     printf("Number: ");
+//     scanf("%i", &n);
+//     printf("Number: %i\n", n);
+// }
 
-char *str(void)
+int main(void)
 {
     char *s;
     printf("String: ");
     scanf("%s", &s);
-    printf("String: )
+    printf("String: %s\n", s)
 }
