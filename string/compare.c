0c8758e2432791d0f794517573e40e68b3a0ef9d
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-24 03:52:09 +0000

    Wed, Jan 24, 2024, 9:22 AM +05:30

--- string/compare.c
+++ string/compare.c
@@ -2,32 +2,20 @@
 #include <cs50.h>
 #include <string.h>
 
-int num(int n1, int n2);
 
 int main(void)
 {
-    printf("For Compare Strings.");
+    printf("For Compare Strings.\n");
     char *a = get_string("A: ");
     char *b = get_string("B: ");
 
-    printf("For Compare Numbers");
-    int n1 = get_int("N1: ");
-    int n2 = get_int("N2: ");
-
     if (strcmp(a, b) == 0)
     {
-        printf("Same.\n:");
+        printf("Same.\n");
     }
     else
     {
         printf("Different.\n");
     }
 
-    printf("Numbers: %i\n", num(n1, n2));
-}
-
-// For numbers
-int num(int n1, int n2)
-{
-    printf("N1 and N2: %i %i \n", n1, n2);
 }
