59d819d75610f782f1cfa861b61e849cc2b4c9c1
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-25 06:36:00 +0000

    Thu, Jan 25, 2024, 12:06 PM +05:30

--- Lecture-1/compare.c
+++ Lecture-1/compare.c
@@ -9,13 +9,16 @@ int main(void)
     if (x > y)
     {
         printf("X is greater than y.\n");
+        return 0;
     }
     else if (x < y)
     {
         printf("X is less than y.\n");
+        return 0;
     }
     else if (x == y)
     {
         printf("X is equal to y.\n");
+        return 0;
     }
 }
