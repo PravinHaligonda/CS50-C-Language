98e6c752ce93d21e6f6eae56f93001473c78c8df
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-25 08:33:02 +0000

    Thu, Jan 25, 2024, 2:03 PM +05:30

---
+++ Lecture-1/calculator.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int add(int a, int b);
+
+int main(void)
+{
+    int x = get_int("X: ");
+    int y = get_int("Y: ");
+
+    int z = add(x, y);
+    printf("X + Y: %i\n", z);
+
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
