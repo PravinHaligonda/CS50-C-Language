c0e7f9d459e787a9df60b55683f0db9c8b1e09f2
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-25 10:44:43 +0000

    Thu, Jan 25, 2024, 4:14 PM +05:30

--- Lecture-1/mario.c
+++ Lecture-1/mario.c
@@ -5,7 +5,11 @@ int main(void)
 {
     for (int i = 0; i < 3; i++)
     {
-        
+        for (int j = 0; i < 3; i++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 
 }
