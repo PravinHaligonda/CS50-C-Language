03ff7d882ab439f84532f08eafa0b162ed731ef6
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-25 12:48:35 +0000

    Thu, Jan 25, 2024, 6:18 PM +05:30

--- Lecture-1/derivative.c
+++ Lecture-1/derivative.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
+float f(float x);
+
 int main(void)
 {
-    int f;
-    int h;
-    int x;
+    float func = f(3);
+    printf("Data=(%f)\n", func);
+}
 
-    
+float f(float x)
+{
+    return 3*x**2 - 4*x + 5;
 }
