0aa4645684a0c77664fe3d554d77111095a55e7c
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-25 13:15:52 +0000

    Thu, Jan 25, 2024, 6:45 PM +05:30

--- Lecture-1/derivative.c
+++ Lecture-1/derivative.c
@@ -5,11 +5,17 @@ float f(float x);
 
 int main(void)
 {
-    float func = f(3);
-    printf("Data=(%f)\n", func);
+    // (f(x + h) - f(x)) / h
+    float h = 0.0001;
+    float x = 3.0;
+
+    float func = (f(x + h) - f(x)) / h;
+    printf("f(x + h)=%f\n", func);
+
 }
 
 float f(float x)
 {
     return 3 * (x * x) - (4 * x) + 5;
 }
+
