ad556a2e428ea2a9ad81eab2498e9371ca4f5b20
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-27 13:07:14 +0000

    Sat, Jan 27, 2024, 6:37 PM +05:30

--- Lecture-3-Algorithms/search.c
+++ Lecture-3-Algorithms/search.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+
+// int main(void)
+// {
+//     int arr[] = {100, 50, 200, 500, 2, 1, 15};
+
+//     int n = get_int("Search: ");
+//     for (int i = 0; i < sizeof(arr); i++)
+//     {
+//         if (arr[i] == n)
+//         {
+//             printf("Found\n");
+//             return 0;
+//         }
+//     }
+//     printf("Not found\n");
+// }
 
 int main(void)
 {
-    int arr[] = {100, 50, 200, 500, 2, 1, 15};
+    string names[] = {"table", "phone", "laptop", "mouse"};
 
-    int n = get_int("Search: ");
-    for (int i = 0; i < sizeof(arr); i++)
+    string input = get_string("Search: ");
+    for (int i = 0; i < 4; i++)
     {
-        if ()
+        if (strcmp(names[i], input) == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
     }
+    printf("Not found\n");
 }
