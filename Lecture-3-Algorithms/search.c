a275fd932fc4ff040b6b5d6fd9c02cdad549f601
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-27 12:59:34 +0000

    Sat, Jan 27, 2024, 6:29 PM +05:30

---
+++ Lecture-3-Algorithms/search.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int arr[] = {100, 50, 200, 500, 2, 1, 15};
+
+    int n = get_int("Search: ");
+    for (int i = 0; i < sizeof(arr); i++)
+    {
+        if ()
+    }
+}
