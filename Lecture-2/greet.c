e39e27d8886540816d27262c8efe94485a5671b3
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-27 09:40:00 +0000

    Sat, Jan 27, 2024, 3:10 PM +05:30

--- Lecture-2/greet.c
+++ Lecture-2/greet.c
@@ -2,7 +2,15 @@
 #include <cs50.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+int main(int argc, string argv[])
 {
-    
+    if (argc == 2)
+    {
+        printf("Hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("Hello world\n");
+    }
+
 }
