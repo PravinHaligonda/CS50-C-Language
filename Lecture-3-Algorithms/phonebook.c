59c11ad7aae3b82778a6d1bd50171ad666ffe957
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-27 16:41:14 +0000

    Sat, Jan 27, 2024, 10:11 PM +05:30

--- Lecture-3-Algorithms/phonebook.c
+++ Lecture-3-Algorithms/phonebook.c
@@ -2,20 +2,35 @@
 #include <cs50.h>
 #include <string.h>
 
+typedef struct
+{
+    string name;
+    string number;
+} person;
 
 int main(void)
 {
-    string names[] = {"John", "Carter", "David", "Emma"};
-    string numbers[] = {"7070707070", "5689452389", "6598457823", "8978456512"};
+    // string names[] = {"John", "Carter", "David", "Emma"};
+    // string numbers[] = {"7070707070", "5689452389", "6598457823", "8978456512"};
 
+    person people[3];
+    people[0].name = "John";
+    people[0].number = "7070707070";
+
+    people[1].name = "Carter";
+    people[1].number = "5689452389";
+
+    people[2].name = "David";
+    people[2].number = "6598457823";
+
+    printf("Size%i\n", sizeof(people));
     string name = get_string("Enter the name: ");
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
     {
-        if (strcmp(names[i], name) == 0)
+        if (strcmp(people[i].name, name) == 0)
         {
-            printf("Name: %s\n", names[i]);
-            printf("Number: %s\n", numbers[i]);
+            printf("Number: %s\n", people[i].number);
             return 0;
         }
     }
