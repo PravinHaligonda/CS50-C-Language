e320f8602b99a8bc1f86344d899b75fcb551e9d2
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-01-27 13:33:03 +0000

    Sat, Jan 27, 2024, 7:03 PM +05:30

--- Lecture-3-Algorithms/search.c
+++ Lecture-3-Algorithms/search.c
@@ -2,22 +2,25 @@
 #include <cs50.h>
 #include <string.h>
 
-// int main(void)
-// {
-//     int arr[] = {100, 50, 200, 500, 2, 1, 15};
+/*
+int main(void)
+{
+    int arr[] = {100, 50, 200, 500, 2, 1, 15};
 
-//     int n = get_int("Search: ");
-//     for (int i = 0; i < sizeof(arr); i++)
-//     {
-//         if (arr[i] == n)
-//         {
-//             printf("Found\n");
-//             return 0;
-//         }
-//     }
-//     printf("Not found\n");
-// }
+    int n = get_int("Search: ");
+    for (int i = 0; i < sizeof(arr); i++)
+    {
+        if (arr[i] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+}
+*/
 
+/*
 int main(void)
 {
     string names[] = {"table", "phone", "laptop", "mouse"};
@@ -32,4 +35,25 @@ int main(void)
         }
     }
     printf("Not found\n");
+}
+*/
+
+int main(void)
+{
+    string names[] = {"John", "Carter", "David", "Emma"};
+    string numbers[] = {"7070707070", "5689452389", "6598457823", "8978456512"};
+
+    string name = get_string("Enter the name: ");
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Name: %s\n", names[i]);
+            printf("Number: %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+
 }
