211bb369879087f59051b0a4be0070e85bc4a3eb
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-03-10 08:31:20 +0000

    Sun, Mar 10, 2024, 2:01 PM +05:30

--- cpu/cycle.c
+++ cpu/cycle.c
@@ -3,5 +3,20 @@
 
 int main(void)
 {
-    printf("The time is now %li.\n", time(NULL));
+    int calculations = 0;
+    clock_t start_time, current_time;
+
+    start_time = clock();
+
+    while(1) {
+        current_time = clock();
+        double elapsed_time = (double)(current_time - start_time) / CLOCKS_PER_SEC;
+
+        if (elapsed_time >= 1.0) {
+            break;
+        }
+        calculations++;
+    }
+    printf("Calculations done: %d\n", calculations);
+    return 0;
 }
