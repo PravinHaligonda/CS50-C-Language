4dc2cc834ed8611090f418012f02021291d3d94e
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-18 16:39:36 +0000

    Tue, Jun 18, 2024, 10:09 PM +05:30

--- Lecture-1-C/Practice Set/ps3.c
+++ Lecture-1-C/Practice Set/ps3.c
@@ -1,7 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
     int num = get_int("Guess a number between 1 to 100: ");
     printf("%i\n", num);
+    long rand = random();
+    printf("Random: %lu\n", rand);
 }
