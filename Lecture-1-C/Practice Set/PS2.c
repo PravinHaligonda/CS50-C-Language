d9b19fc9c4100be5b62b8188d9e991c23371afe8
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-18 16:24:04 +0000

    Tue, Jun 18, 2024, 9:54 PM +05:30

--- Lecture-1-C/Practice Set/PS2.c
+++ Lecture-1-C/Practice Set/PS2.c
@@ -2,8 +2,11 @@
 #include <cs50.h>
 
 int main(void) {
+
     printf("Fahrenheit to Celcius.\n");
+
     int F = get_int("F:");
     float out = (F - 32) * 5 / 9;
+
     printf("%f Celcius\n", out);
 }
