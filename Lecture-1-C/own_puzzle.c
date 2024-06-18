b73fdfdfb6fbf50ff5b9a0fc7046fed333630264
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-18 06:35:28 +0000

    Tue, Jun 18, 2024, 12:05 PM +05:30

--- Lecture-1-C/own_puzzle.c
+++ Lecture-1-C/own_puzzle.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// Prototype
 void meow(int n);
 
+// Main Function
 int main(void) {
     meow(5);
 }
 
+// arguments: int n
+// Returns: Nothing
 void meow(int n) {
     for (int i = 0; i < n; i++) {
         printf("Meow\n");
