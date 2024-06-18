247ab2154c740acd5bdec93236a62da3983ad9a8
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-18 04:20:30 +0000

    Tue, Jun 18, 2024, 9:50 AM +05:30

--- Lecture-1-C/hello.c
+++ Lecture-1-C/hello.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <cs50.h>
 
-int main(void)
-{
-    string name = get_string("Name: ");
-    printf("Hello, %s\n", name);
+int main(void) {
+    printf("Hello, World\n");
 }
+-
