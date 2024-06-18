f07cc130ded8ab268e7d2946aaaa1fdcabe6b77d
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-18 06:16:35 +0000

    Tue, Jun 18, 2024, 11:46 AM +05:30

--- Lecture-1-C/for.c
+++ Lecture-1-C/for.c
@@ -1,8 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int loop(void);
+
 int main(void) {
-    for (int i = 0; i < 0; i++) {
-        printf("Meow\n");
+    loop();
+}
+
+int loop(void) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            print("#");
+        }
+        print("\n");
     }
 }
