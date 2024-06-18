eeb690031a93afe1a7ec44e6a9b767d061beb04a
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-18 16:49:52 +0000

    Tue, Jun 18, 2024, 10:19 PM +05:30

--- Lecture-1-C/Practice Set/ps3.c
+++ Lecture-1-C/Practice Set/ps3.c
@@ -7,7 +7,8 @@ int main(void) {
 
     int num = get_int("Guess a number between 1 to 100: ");
     printf("%i\n", num);
-    
+
     float rand = random();
     printf("Random: %f\n", rand);
+    
 }
