f0f065d9a5d360d8738f095403b44117fcad7f71
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-20 04:28:57 +0000

    Thu, Jun 20, 2024, 9:58 AM +05:30

--- arg.c
+++ arg.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void greet(void) {
-    printf("Hello, world");
+void error_usage() {
+    printf()
 }
 
 int main(int argc, string argv[]) {
-    if (argv[1] == 'g'){
-        greet();
-    }
-    else
-    {
-        print("Invalid Command");
-    }
-}
 
+}
