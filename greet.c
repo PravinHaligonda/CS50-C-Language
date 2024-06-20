306acb57ee6fe78f70b3ce06b238f8a32f13ad18
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-20 03:27:55 +0000

    Thu, Jun 20, 2024, 8:57 AM +05:30

---
+++ greet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[]) {
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s\n", answer);
+    printf("%i\n", argc);
+    printf("%s\n", argv);
+}
