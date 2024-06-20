e351c9b7f416315d376da2fe30c5ffa73b6dedd8
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-20 07:06:57 +0000

    Thu, Jun 20, 2024, 12:36 PM +05:30

--- arg.c
+++ arg.c
@@ -3,11 +3,7 @@
 #include <string.h>
 
 int main(int argc, string argv[]) {
-    if (argc < 2) {
-        printf("Hello, %s\n", argv[1]);
-    }
-    else
-    {
-        printf("Invalid command");
-    }
+    // Print the number of command line arguments.
+    
+    printf("Arguments typed in CLI: %i\n", len)
 }
