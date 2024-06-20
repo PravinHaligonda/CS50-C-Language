cefe1506a43af653e275c7ca6e76c6f91903673b
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-20 03:50:01 +0000

    Thu, Jun 20, 2024, 9:20 AM +05:30

--- greet.c
+++ greet.c
@@ -4,11 +4,13 @@
 int main(int argc, string argv[]) {
     if (argc == 2)
     {
-    printf("Hello, %s\n", argv[1]);
+        printf("Hello, %s\n", argv[1]);
+        return 1;
     }
     else
     {
         printf("Hello World\n");
+        return 3;
     }
 }
 
