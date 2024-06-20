a18448f59a52ffc1b707d86fb88ebe20a2ed828c
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-20 04:37:19 +0000

    Thu, Jun 20, 2024, 10:07 AM +05:30

--- arg.c
+++ arg.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void error_usage() {
+void error_usage(void) {
     printf("Usage:   run \n");
 }
 
 int main(int argc, string argv[]) {
-    if (argc >= 2) {printf("%s\n", argv[1]);} else { error_usage(); }
+    if (argc < 2) {
+        printf("%s\n", argv[1]);
+    }
+    else
+    {
+        error_usage();
+    }
 }
