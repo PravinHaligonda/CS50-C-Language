e9fb9c171239d860abf01d3470f0db7c088f843c
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-22 05:55:27 +0000

    Sat, Jun 22, 2024, 11:25 AM +05:30

--- addresses.c
+++ addresses.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
+void point();
+
 int main(void) {
     int n = 50;
     int *p = &n;
     printf("%p\n", &n);
     printf("%i\n", *p);
     printf("%p\n", &p);
+    point();
 }
 
 // & is address of
 // * is dereference operator (like taken address and go to it.)
+
+void point() {
+    int s = 3;
+    int * p = &s;
+
+    printf("%i\n", *p);
+    printf("%p\n", &p)
+}
