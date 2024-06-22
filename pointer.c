59bfcdae7b14c683d1d706e59ec58d9e107aa667
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-22 17:12:28 +0000

    Sat, Jun 22, 2024, 10:42 PM +05:30

--- pointer.c
+++ pointer.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+// Prototype
+void arithmatic(void);
+
 int main(void) {
     char *s = "HI!";
     printf("%c", s[0]);
     printf("%c", s[1]);
     printf("%c\n", s[2]);
+    arithmatic();
+}
+
+// pointer arithmatic
+void arithmatic(void) {
+    char *p = "PRAVIN";
+    printf("%c", *p);
+    printf("%c", *(p + 1));
+    printf("%c", *(p + 2));
+    printf("%c", *(p + 3));
+    printf("%c", *(p + 4));
+    printf("%c\n", *(p + 5));
 }
