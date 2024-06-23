1fc46d0ce25d342d3401da3597a3a1a051bf74f9
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-23 03:31:42 +0000

    Sun, Jun 23, 2024, 9:01 AM +05:30

---
+++ copy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+    char *t = s;
+
+    t[0] = toupper(t[0]);
+
+    printf("%s\n", s);
+    printf("%s\n", t);
+}
