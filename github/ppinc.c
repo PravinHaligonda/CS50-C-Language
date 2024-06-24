2833bb71a428db9a919fd4a98bbb653f100e751c
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-24 12:46:36 +0000

    Mon, Jun 24, 2024, 6:16 PM +05:30

--- github/ppinc.c
+++ github/ppinc.c
@@ -13,7 +13,7 @@ void pre_post_increment(void) {
 
     num1 = 10;
     num2 = ++num1;
-    printf("Num1: %i Num2: %i\n", num1, num2);
+    // Value of num1 is incremented by 1 and then assigned to 
 
 }
 
