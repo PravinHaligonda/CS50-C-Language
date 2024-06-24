b102044f3165c7c2e118c80ca42ff7b98299e9ba
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-24 13:12:26 +0000

    Mon, Jun 24, 2024, 6:42 PM +05:30

--- github/loops.c
+++ github/loops.c
@@ -4,10 +4,17 @@ int arr[5];
 
 int main(void) {
 
-    for (int i = 0; i < sizeof(arr); i++) {
+    // int = 4 bytes
+    // prints 20 because 4 bytes * 5 of them = 20 bytes
+    printf("Size of arr: %lu\n", sizeof(arr));
+
+    for (int i = 0; i < 5; i++) {
         arr[i] = i + 1;
     }
-    printf(arr[]);
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", arr[i]);
+    }
 
 
 }
