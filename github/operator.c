540d7775b49462507b49c42a9adba08dc017169e
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-24 12:33:56 +0000

    Mon, Jun 24, 2024, 6:03 PM +05:30

---
+++ github/operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int score = 100;
+
+    if (score == 100) {
+        printf("Perfect Score.");
+    }
+    else {
+        printf("Needs Improvement.");
+    }
+}
