27797f3c48740787444e0e795c1a84e26055429d
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-24 12:48:45 +0000

    Mon, Jun 24, 2024, 6:18 PM +05:30

--- github/ppinc.c
+++ github/ppinc.c
@@ -7,13 +7,13 @@ void pre_post_increment(void) {
     int num1;
     int num2;
 
-    num1 = 10;
+    num1 = 10; //This is going to change
     num2 = num1++;
-    printf("Num1: %i and Num2: %i\n", num1, num2);
+    // Value of num1 assigned to num2 then it incremented by 1
 
-    num1 = 10;
+    num1 = 10; //both num1 and num2 going to change.
     num2 = ++num1;
-    // Value of num1 is incremented by 1 and then assigned to 
+    // Value of num1 is incremented by 1 and then assigned to num2
 
 }
 
