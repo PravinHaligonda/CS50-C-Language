8425590a49fb21b289687070b5e0780f82ca2452
hpravin <114744214+PravinHaligonda@users.noreply.github.com>
2024-06-25 16:39:07 +0000

    Tue, Jun 25, 2024, 10:09 PM +05:30

--- Python/scores.py
+++ Python/scores.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 scores = []
 
 for i in range(3):
-    score = get_int(f"scores{i + 1}: ")
+    score = get_int(f"scores {i + 1}: ")
     scores.append(score)
 
 average = sum(scores) / len(scores)
