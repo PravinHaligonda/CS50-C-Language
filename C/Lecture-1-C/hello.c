c0435a6db318b86b48bd9772a5f42e33e5c367ae
Pravin Bhimgonda Haligonda <114744214+PravinHaligonda@users.noreply.github.com>
2024-09-22 07:24:42 +0000

    Sun, Sep 22, 2024, 12:54 PM +05:30

--- C/Lecture-1-C/hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void) {
-    string answer = get_string("Enter Name:");
-    printf("Hello, %s\n", answer);
-}
--- C/Practice Set/PS1.c
+++
@@ -1,8 +0,0 @@
-// Hello world enhanced
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void) {
-    string name = get_string("Enter your name: ");
-    printf("Hello, %s\n", name);
-}
