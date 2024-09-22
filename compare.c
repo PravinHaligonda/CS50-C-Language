f1e7494758a1b7fcebc882155d0908a19f2436a1
Pravin Bhimgonda Haligonda <114744214+PravinHaligonda@users.noreply.github.com>
2024-09-22 08:57:25 +0000

    Sun, Sep 22, 2024, 2:27 PM +05:30

--- compare.c
+++ compare.c
@@ -3,5 +3,19 @@
 
 int main(void)
 {
-    
+    int x = get_int("What's X? ");
+    int y = get_int("What's Y? ");
+
+    if (x < y)
+    {
+        printf("x is less than y.\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y.\n");
+    }
+    else
+    {
+        printf("x is equal to y.\n");
+    }
 }
